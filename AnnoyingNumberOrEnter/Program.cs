ec906c654a636a6c8dfb268c1b83414a00db6514
17193 <asialevesquegault@gmail.com>
2021-01-14 12:17:29 -0700

    while loop that adds and interacts with command line

--- AnnoyingNumberOrEnter/Program.cs
+++ AnnoyingNumberOrEnter/Program.cs
@@ -9,7 +9,7 @@ namespace AnnoyingNumberOrEnter
             // Write a program and continuously ask the user to enter a number
             // or "ok" to exit. Calculate the sum of all the previously entered
             // numbers and display it on the console.
-            //Ask for number or ok to exit
+            //Ask for number or ok to exit.
 
             int total = 0;
             while (true)
